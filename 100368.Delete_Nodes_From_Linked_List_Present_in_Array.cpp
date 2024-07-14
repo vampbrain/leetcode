8f0551a027b559b19c0af1922a831c31c80984a7
vampbrain <91673458+vampbrain@users.noreply.github.com>
2024-07-14 08:37:16 +0530

    Create 100368.Delete_Nodes_From_Linked_List_Present_in_Array.cpp

---
+++ 100368.Delete_Nodes_From_Linked_List_Present_in_Array.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* modifiedList(vector<int>& nums, ListNode* head) {
+        
+        unordered_set<int> numset(nums.begin(),nums.end());
+        
+        ListNode* dummy = new ListNode(0);
+        dummy->next = head;
+        ListNode* current = dummy;
+        
+        while (current->next!=nullptr){
+            if (numset.count(current->next->val)){
+                ListNode* temp = current->next;
+                current->next = current->next->next;
+                delete temp;
+            } else{
+                current = current->next;
+            }
+        }
+        ListNode* newHead = dummy->next;
+        delete dummy;
+        return newHead;
+        
+    }
+};
